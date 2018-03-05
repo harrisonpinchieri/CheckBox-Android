1d52e6936ace7fa03cc202668bbca20dee790c18
Harrison <harrisonpinchieri@hotmail.com>
2018-03-05 14:26:17 -0300

    Initial commit

---
+++ app/src/main/java/checkbox2/cursoandroid/com/checkbox2/MainActivity.java
@@ -0,0 +1,47 @@
+package checkbox2.cursoandroid.com.checkbox2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox checkBoxCao;
+    private CheckBox checkBoxGato;
+    private CheckBox checkBoxPapagaio;
+
+
+    private Button botaoMostrar;
+    private TextView textoExibicao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkBoxCao = findViewById(R.id.checkBoxCao);
+        checkBoxGato  = findViewById(R.id.checkBoxGato);
+        checkBoxPapagaio = findViewById(R.id.checkBoxPapagaio);
+
+        textoExibicao = findViewById(R.id.textoExibicaoId);
+        botaoMostrar = findViewById(R.id.botaoMostrarId);
+
+        botaoMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itensSelecionados = "";
+
+            itensSelecionados += "Item:  "+checkBoxCao.getText()+ "Status: "+ checkBoxCao.isChecked() + "\n";
+            itensSelecionados += "Item:  "+checkBoxGato.getText()+ "Status: "+ checkBoxGato.isChecked() + "\n";
+            itensSelecionados += "Item:  "+checkBoxPapagaio.getText()+ "Status: "+ checkBoxPapagaio.isChecked() + "\n";
+
+                textoExibicao.setText(itensSelecionados);
+
+            }
+        });
+
+    }
+}
